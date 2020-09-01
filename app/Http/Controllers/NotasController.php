b5e8a10088ca5f306202c1f2255d20d696a0cf21
Priscila <priscilagrazieleba@gmail.com>
2020-08-31 23:08:55 -0300

    added auth

--- app/Http/Controllers/NotasController.php
+++ app/Http/Controllers/NotasController.php
@@ -10,6 +10,11 @@ use App\CalculadorMedia;
 
 class NotasController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         $notas = Nota::all();
